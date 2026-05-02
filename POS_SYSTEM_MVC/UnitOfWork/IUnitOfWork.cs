fe86c19d6aeddc8b99364afa10283f65eacdd45b
Mahmoud Adel <mahmoud.adeli7888@gmail.com>
2026-05-03 01:27:52 +0300

    feat: added base for unit of work

---
+++ POS_SYSTEM_MVC/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace POS_SYSTEM_MVC.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
---
+++ POS_SYSTEM_MVC/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.EntityFrameworkCore.Storage;
+using POS_SYSTEM_MVC.Data;
+
+namespace POS_SYSTEM_MVC.UnitOfWork
+{
+    public class UnitOfWork(POSContext context) : IUnitOfWork
+    {
+        private readonly POSContext _context = context;
+        private IDbContextTransaction? _transaction;
+
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            if (_transaction is null) throw new InvalidOperationException("No transaction in progress.");
+            await _transaction.CommitAsync();
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            if (_transaction is null) return;
+            await _transaction.RollbackAsync();
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
+            
+        public void Dispose()
+        {
+            _transaction?.Dispose();
+            _context.Dispose();
+        }
+    }
+}
