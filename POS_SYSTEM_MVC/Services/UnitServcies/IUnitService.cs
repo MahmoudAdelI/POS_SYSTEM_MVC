b6c3d15654659e6576c94d50b75c5bad04733f42
NourSobhy1 <noursobhy222@gmail.com>
2026-05-06 19:11:21 +0300

    edit Unitservices
    edit Unitservices

--- POS_SYSTEM_MVC/Services/UnitServcies/IUnitService.cs
+++ POS_SYSTEM_MVC/Services/UnitServcies/IUnitService.cs
@@ -1,7 +1,13 @@
-﻿namespace POS_SYSTEM_MVC.Services.UnitServices
+﻿using POS_SYSTEM_MVC.Models;
+namespace POS_SYSTEM_MVC.Services.UnitServices
 
 {
     public interface IUnitService
     {
+        Task<IEnumerable<Unit>> GetAllAsync();
+        Task<Unit?> GetByIdAsync(int id);
+        Task AddAsync(Unit unit);
+        Task UpdateAsync(Unit unit);
+        Task DeleteAsync(int id);
     }
 }
