9d75a267cf663d049d1e1329c976919df5e14fc8
Muhamed El-Dfrawy <77486718+MOW-Dfrawy@users.noreply.github.com>
2026-05-07 18:31:57 +0300

    Add admin area and account_manger controller

---
+++ POS_SYSTEM_MVC/ViewModels/LoginVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POS_SYSTEM_MVC.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        public string Username { get; set; }
+
+        //[Required]
+        //[EmailAddress]
+        //public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; } = false;
+
+    }
+}
