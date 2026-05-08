99e38a9218654cc6ce3d70c7f81fe566bcdcf87d
NourSobhy1 <noursobhy222@gmail.com>
2026-05-08 17:13:09 +0300

    Dashboard
    Dashboard

---
+++ POS_SYSTEM_MVC/DTOs/Dashboard/RecentSaleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace POS_SYSTEM_MVC.DTOs.Dashboard
+{
+    public class RecentSaleDto
+    {
+        public string ProductName { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; } // "Completed" or "Pending"
+        public DateTime Date { get; set; }
+    }
+}
