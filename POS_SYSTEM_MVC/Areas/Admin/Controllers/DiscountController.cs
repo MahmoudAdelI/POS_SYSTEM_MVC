14cce894015b5e105ed109fcc15bcdb64d81236d
Blackdukee <1213059@dtu.edu.eg>
2026-05-14 00:10:12 +0300

    fix(di): remove unregistered repositories from DiscountController constructor

--- POS_SYSTEM_MVC/Areas/Admin/Controllers/DiscountController.cs
+++ POS_SYSTEM_MVC/Areas/Admin/Controllers/DiscountController.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using POS_SYSTEM_MVC.Constants;
-using POS_SYSTEM_MVC.Repositories.DiscountRepo;
-using POS_SYSTEM_MVC.Repositories.ProductRepo;
-using POS_SYSTEM_MVC.Repositories.ProductVariantRepo;
 using POS_SYSTEM_MVC.Services.DiscountServices;
+using POS_SYSTEM_MVC.UnitOfWork;
 using POS_SYSTEM_MVC.ViewModels;
 
 namespace POS_SYSTEM_MVC.Areas.Admin.Controllers
@@ -14,67 +11,51 @@ namespace POS_SYSTEM_MVC.Areas.Admin.Controllers
     [Authorize(Roles = Role.Admin)]
     public class DiscountController : Controller
     {
-        private readonly IDiscountRepository _discountRepository;
-        private readonly IProductRepository _productRepository;
-        private readonly IProductVariantRepository _productVariantRepository;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IDiscountService _discountService;
 
-        public DiscountController(IDiscountRepository discountRepository,IProductRepository productRepository,IProductVariantRepository productVariantRepository, IDiscountService discountService)
+        public DiscountController(IUnitOfWork unitOfWork, IDiscountService discountService)
         {
-            _discountRepository = discountRepository;
-            _productRepository = productRepository;
-            _productVariantRepository = productVariantRepository;
+            _unitOfWork = unitOfWork;
             _discountService = discountService;
         }
-        public async Task< IActionResult> Index()
+
+        public async Task<IActionResult> Index()
         {
-            var discounts =
-                   await _discountRepository.GetAllAsync();
-
-            var products =
-                await _productRepository.GetAllAsync();
-
-            var variants =
-                await _productVariantRepository.GetAllAsync();
+            var discounts = await _unitOfWork.Discounts.GetAllAsync();
+            var products = await _unitOfWork.Products.GetAllAsync();
+            var variants = await _unitOfWork.ProductVariants.GetAllAsync();
 
             var vm = new DiscountformVM
             {
                 Discounts = discounts,
                 Products = products,
-
-                ProductVariants= variants
-
+                ProductVariants = variants
             };
 
             return View(vm);
         }
 
         [HttpPost]
-        public async Task<IActionResult>Index(DiscountformVM vm)
+        public async Task<IActionResult> Index(DiscountformVM vm)
         {
-            var errors =await _discountService.CreateAsync(vm.Discount);
+            var errors = await _discountService.CreateAsync(vm.Discount);
 
             if (errors.Any())
             {
                 foreach (var error in errors)
                 {
-                    ModelState.AddModelError(
-                        "",
-                        error);
+                    ModelState.AddModelError("", error);
                 }
 
-                vm.Discounts =await _discountService.GetAllAsync();
-
-                vm.Products =await _productRepository.GetAllAsync();
-
-                vm.ProductVariants =await _productVariantRepository .GetAllAsync();
+                vm.Discounts = await _discountService.GetAllAsync();
+                vm.Products = await _unitOfWork.Products.GetAllAsync();
+                vm.ProductVariants = await _unitOfWork.ProductVariants.GetAllAsync();
 
                 return View(vm);
             }
 
             return RedirectToAction(nameof(Index));
         }
-
-
     }
 }
