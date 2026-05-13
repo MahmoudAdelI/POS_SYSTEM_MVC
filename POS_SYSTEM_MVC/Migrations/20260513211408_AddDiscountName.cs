070dbdb71d536ea9e2cc1f405d089daafb7fbb66
Blackdukee <1213059@dtu.edu.eg>
2026-05-14 00:14:52 +0300

    fix(db): add missing migration for Discount Name column

---
+++ POS_SYSTEM_MVC/Migrations/20260513211408_AddDiscountName.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POS_SYSTEM_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiscountName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Discounts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Discounts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Discounts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Discounts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Discounts");
+        }
+    }
+}
